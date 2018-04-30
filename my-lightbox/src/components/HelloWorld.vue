e9f69c4d39a768eb1b74fc9b8d35a639f584fe0e
pc <pengchong@hd123.com>
2018-04-30 15:33:06 +0800

    adjust

---
+++ my-lightbox/src/components/HelloWorld.vue
@@ -0,0 +1,79 @@
+<template>
+  <main>
+    <div v-for="(url,index) in urls" class="img-wrapper" @click="onOpenLightbox(urls,index)">
+      <div class="img-box">
+        <img :src="url">
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import Lightbox from './Lightbox';
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      urls: ['/static/1.jpeg', '/static/2.jpeg', '/static/3.jpeg', '/static/4.jpeg','/static/logo.png']
+    }
+  },
+  methods: {
+    onOpenLightbox: function (urls, index) {
+      Lightbox.open({urls: urls, index: index})
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .img-wrapper {
+    padding: 10%;
+    position: relative;
+    display: inline-block;
+    .img-box {
+      width: 100%;
+      height: 100%;
+      left: 0;
+      top: 0;
+      box-sizing: border-box;
+      position: absolute;
+      border: 0.5em solid #eeeeee;
+      cursor: pointer;
+      overflow: hidden;
+      img {
+        max-width: 100%;
+        max-height: 100%;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        margin: auto;
+      }
+    }
+    .img-box:after {
+      content: '点击预览大图';
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      background: rgba(0, 0, 0, 0.6);
+      color: white;
+      font-size: .7em;
+      height: 3em;
+      line-height: 3em;
+      text-align: center;
+      transform: translateY(100%);
+      transition: transform 0.25s;
+    }
+    .img-box:hover {
+      border-color: #409EFF;
+    }
+    .img-box:hover:after {
+      transform: translateY(0);
+    }
+  }
+
+</style>
